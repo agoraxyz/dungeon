4668ac5b8125a8853ac1bf1786dbdb95ca8d3f20
valid <valid@zgen.hu>
2021-12-17 16:52:33 +0100

    Initialize solana

---
+++ next.config.js
@@ -0,0 +1,27 @@
+const withTM = require("next-transpile-modules")([
+  "@blocto/sdk",
+  "@project-serum/sol-wallet-adapter",
+  "@solana/wallet-adapter-base",
+  "@solana/wallet-adapter-react",
+  "@solana/wallet-adapter-phantom",
+])
+
+/** @type {import("next").NextConfig} */
+module.exports = withTM({
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+            replaceAttrValues: { "#fff": "currentColor" },
+          },
+        },
+      ],
+    })
+
+    return config
+  },
+})
