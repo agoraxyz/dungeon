480c4a8d1c4d8c01af0061a03769024634880d9b
Janos Mezo <jmezo.asd@gmail.com>
2021-12-22 13:36:34 +0100

    increased box size for displaying dungeon

--- src/components/index/dungeon/LevelUI.tsx
+++ src/components/index/dungeon/LevelUI.tsx
@@ -47,19 +47,19 @@ export default class Maze extends Component<IProps, IState> {
       for (let j = 0; j < maze[i].length; j++) {
         const cell = maze[i][j]
         if (cell.length === 0) {
-          row.push(<Box key={j} bg="gray.300" w="5px" h="5px" />)
+          row.push(<Box key={j} bg="gray.300" w="10px" h="10px" />)
         } else if (cell.includes("wall")) {
-          row.push(<Box key={j} bg="yellow.900" w="5px" h="5px" />)
+          row.push(<Box key={j} bg="yellow.900" w="10px" h="10px" />)
         } else if (cell.includes("hero")) {
-          row.push(<Box key={j} bg="blue.300" w="5px" h="5px" />)
+          row.push(<Box key={j} bg="blue.300" w="10px" h="10px" />)
         } else if (cell.includes("obj")) {
-          row.push(<Box key={j} bg="green.300" w="5px" h="5px" />)
+          row.push(<Box key={j} bg="green.300" w="10px" h="10px" />)
         } else if (cell.includes("entrance")) {
-          row.push(<Box key={j} bg="gray.0" w="5px" h="5px" />)
+          row.push(<Box key={j} bg="gray.0" w="10px" h="10px" />)
         } else if (cell.includes("ladder")) {
-          row.push(<Box key={j} bg="yellow.300" w="5px" h="5px" />)
+          row.push(<Box key={j} bg="yellow.300" w="10px" h="10px" />)
         } else {
-          row.push(<Box key={j} bg="red.600" w="5px" h="5px" />)
+          row.push(<Box key={j} bg="red.600" w="10px" h="10px" />)
         }
       }
       board.push(
