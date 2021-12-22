ea08e2996f079e01c50e97b273f8c2dd0f0f6763
PopcornPaws <lipschitz77@gmail.com>
2021-12-22 19:06:02 +0100

    Deployed to testnet.

--- src/contract-logic/index.ts
+++ src/contract-logic/index.ts
@@ -5,19 +5,19 @@ import { sendTransaction, PLAYER_SECRET, CONNECTION } from "./test"
 
 ;(async () => {
   const player = Keypair.fromSecretKey(PLAYER_SECRET)
-  //await CONNECTION.confirmTransaction(
-  //  await CONNECTION.requestAirdrop(player.publicKey, 100000000)
-  //)
+  await CONNECTION.confirmTransaction(
+    await CONNECTION.requestAirdrop(player.publicKey, 100000000)
+  )
   // INITIALIZE GAME
-  //const transaction = await initGame(CONNECTION, player.publicKey);
-  //await sendTransaction(transaction, player);
-  //console.log(await getHeroState(CONNECTION, player.publicKey));
-  // PERFORM ACITON
-  console.log(await getGameState(CONNECTION, player.publicKey));
-  const transaction = await performAction(CONNECTION, player.publicKey, 0);
+  const transaction = await initGame(CONNECTION, player.publicKey);
   await sendTransaction(transaction, player);
-  let gameState = await getGameState(CONNECTION, player.publicKey);
-  console.log(gameState.combatLogs)
+  console.log(await getHeroState(CONNECTION, player.publicKey));
+  // PERFORM ACITON
+  //console.log(await getGameState(CONNECTION, player.publicKey));
+  //const transaction = await performAction(CONNECTION, player.publicKey, 0);
+  //await sendTransaction(transaction, player);
+  //let gameState = await getGameState(CONNECTION, player.publicKey);
+  //console.log(gameState.combatLogs)
   // DELETE GAME
   //const transaction = await deleteGame(CONNECTION, player.publicKey);
   //await sendTransaction(transaction, player);
