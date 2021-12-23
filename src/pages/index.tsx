f9f112885294c9da93ea2c1021445f466651e4d0
Janos Mezo <jmezo.asd@gmail.com>
2021-12-24 00:46:46 +0100

    added button to switch between views

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,43 @@
-import { Box, Heading } from "@chakra-ui/react"
+import { Box, Button, Heading } from "@chakra-ui/react"
 import Layout from "components/common/Layout"
 import DebugStuff from "components/index/DebugStuff"
 import { ClickDungeon, Dungeon } from "components/index/dungeon"
+import { useState } from "react"
 
-const Page = (): JSX.Element => (
-  <Layout title="Home">
-    <div>
-      <Heading>debug stuff</Heading>
-      <DebugStuff />
-    </div>
+const Page = (): JSX.Element => {
+  const [which, setWhich] = useState<string>("ClickDungeon")
 
-    <Box display="flex" flexDirection="row">
-      <Box padding="10px">
-        <Heading>Dungeon</Heading>
-        <Dungeon />
+  const changeDungeon = () => {
+    if (which === "Dungeon") {
+      setWhich("ClickDungeon")
+    } else {
+      setWhich("Dungeon")
+    }
+  }
+
+  return (
+    <Layout title="Home">
+      <div>
+        <Heading>debug stuff</Heading>
+        <DebugStuff />
+      </div>
+
+      <Box display="flex" flexDirection="column">
+        <Button onClick={changeDungeon}>switch</Button>
+        {which === "ClickDungeon" ? (
+          <Box padding="10px">
+            <Heading>ClickDungeon</Heading>
+            <ClickDungeon />
+          </Box>
+        ) : (
+          <Box padding="10px">
+            <Heading>Dungeon</Heading>
+            <Dungeon />
+          </Box>
+        )}
       </Box>
-      <Box padding="10px">
-        <Heading>ClickDungeon</Heading>
-        <ClickDungeon />
-      </Box>
-    </Box>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default Page
