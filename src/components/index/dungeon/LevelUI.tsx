1cee3969bda2dd4549b8bfd1771d8ac7a8d41d6c
Janos Mezo <jmezo.asd@gmail.com>
2021-12-30 18:05:45 +0100

    updated levelui to fetch and show contract data

--- src/components/index/dungeon/LevelUI.tsx
+++ src/components/index/dungeon/LevelUI.tsx
@@ -1,47 +1,81 @@
-import { Box } from "@chakra-ui/react"
-import { Component } from "react"
+import { Box, Button, Heading, Image, Text } from "@chakra-ui/react"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import * as query from "contract-logic/queries"
+import * as schema from "contract-logic/schema"
+import * as txs from "contract-logic/transactions"
+import { useState } from "react"
+import * as print from "./dungeonInfo"
 import Level from "./Level"
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface IProps {}
+const LevelUi = () => {
+  const level = new Level()
+  const [_level] = useState<Level | undefined>(level)
+  const [hero, setHero] = useState<schema.Hero | undefined>(undefined)
+  const [dungeonState, setDungeonState] = useState<schema.DungeonState | undefined>(
+    undefined
+  )
+  const [_actionNum, _setActionNum] = useState<number | undefined>(undefined)
 
-interface IState {
-  level: Level
-}
+  const { connection } = useConnection()
+  const { publicKey, sendTransaction } = useWallet()
 
-export default class Maze extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    const level = new Level()
-    super(props)
-    this.state = {
-      level: level,
+  const [_fVal, _setFval] = useState<number>(0)
+  const _forceUpdate = () => {
+    _setFval(_fVal + 1)
+  }
+
+  const initGame = async () => {
+    const tx = await txs.initGame(connection, publicKey)
+    const res = await sendTransaction(tx, connection)
+    await connection.confirmTransaction(res, "confirmed")
+    console.log("initialized game")
+    await fetchHero()
+    await fetchDungeonState()
+  }
+
+  const deleteGame = async () => {
+    const tx = await txs.deleteGame(connection, publicKey)
+    const res = await sendTransaction(tx, connection)
+    await connection.confirmTransaction(res, "confirmed")
+    console.log("deleted game")
+    setDungeonState(undefined)
+    setHero(undefined)
+  }
+
+  const performAction = async (actionId: number) => {
+    console.log("performing action", actionId)
+    const tx = await txs.performAction(connection, publicKey, actionId)
+    const res = await sendTransaction(tx, connection)
+    await connection.confirmTransaction(res, "confirmed")
+    await fetchHero()
+    await fetchDungeonState()
+  }
+
+  const fetchHero = async () => {
+    const _hero = await query.getHeroState(connection, publicKey)
+    if (_hero === undefined) {
+      console.log("hero not initialized")
+      setHero(undefined)
     }
-    this.moveNorth = this.moveNorth.bind(this)
-    this.moveSouth = this.moveSouth.bind(this)
-    this.moveEast = this.moveEast.bind(this)
-    this.moveWest = this.moveWest.bind(this)
-    this.handleKeyPress = this.handleKeyPress.bind(this)
+    setHero(_hero)
   }
 
-  moveNorth() {
-    this.state.level.moveNorth()
-    this.setState({ level: this.state.level })
-  }
-  moveSouth() {
-    this.state.level.moveSouth()
-    this.setState({ level: this.state.level })
-  }
-  moveEast() {
-    this.state.level.moveEast()
-    this.setState({ level: this.state.level })
-  }
-  moveWest() {
-    this.state.level.moveWest()
-    this.setState({ level: this.state.level })
+  const fetchDungeonState = async () => {
+    const state = await query.getGameState(connection, publicKey)
+    if (state === undefined) {
+      console.log("game not initialized")
+      setDungeonState(undefined)
+    }
+    setDungeonState(state)
   }
 
-  renderLevel() {
-    const maze = this.state.level.map
+  const fetchGameState = async () => {
+    await fetchHero()
+    await fetchDungeonState()
+  }
+
+  const renderLevel = () => {
+    const maze = _level.map
     const board = []
     for (let i = 0; i < maze.length; i++) {
       const row = []
@@ -72,26 +106,89 @@ export default class Maze extends Component<IProps, IState> {
     return board
   }
 
-  handleKeyPress(e) {
-    if (e.key === "w") {
-      this.moveNorth()
-    } else if (e.key === "s") {
-      this.moveSouth()
-    } else if (e.key === "a") {
-      this.moveWest()
-    } else if (e.key === "d") {
-      this.moveEast()
-    } else if (e.key == "f") {
-      this.state.level.interact()
-      this.setState({ level: this.state.level })
-    }
-  }
+  const showHero = () =>
+    hero ? (
+      <Box>
+        <Heading size="lg">Hero</Heading>
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Viktor_Orb%C3%A1n_%2840462383173%29.jpg/1200px-Viktor_Orb%C3%A1n_%2840462383173%29.jpg"
+          alt="hero"
+          height="200px"
+          width="150px"
+        />
+        {print.heroInList(hero).map((h, i) => (
+          <Text key={i}>{h}</Text>
+        ))}
+      </Box>
+    ) : (
+      <Box>
+        <Heading size="lg">Hero</Heading>
+      </Box>
+    )
 
-  render() {
+  const showAction = () => {
+    const key = _actionNum
+    if (key === undefined) return <Box>nothing selected</Box>
+    const action = dungeonState.actionSpace.get(key)
     return (
-      <Box onKeyDown={this.handleKeyPress} tabIndex={0}>
-        {this.renderLevel()}
+      <Box key={key} display="flex" flexDir="column" alignItems="center">
+        {/* <Image src={`/creatures/1.png`} alt="asd" /> */}
+        <Text>action {key}</Text>
+        <Box display="flex" alignItems="baseline">
+          <Box>
+            {print.actionInfoInList(action).map((a, i) => (
+              <Text key={i}>{a}</Text>
+            ))}
+          </Box>
+        </Box>
       </Box>
     )
   }
+
+  const handleKeyPress = (e) => {
+    if (e.key === "w") {
+      _level.moveNorth()
+      _forceUpdate()
+    } else if (e.key === "s") {
+      _level.moveSouth()
+      _forceUpdate()
+    } else if (e.key === "a") {
+      _level.moveWest()
+      _forceUpdate()
+    } else if (e.key === "d") {
+      _level.moveEast()
+      _forceUpdate()
+    } else if (e.key === "i") {
+      if (!dungeonState) return
+      _setActionNum(0)
+    } else if (e.key == "f") {
+      _level.interact()
+      _forceUpdate()
+    }
+  }
+
+  return (
+    <Box>
+      <Box display="flex" flexDir="row">
+        <Button colorScheme="blue" onClick={fetchGameState}>
+          refresh gameState
+        </Button>
+        <Button colorScheme="blue" onClick={initGame}>
+          init game
+        </Button>
+        <Button colorScheme="blue" onClick={deleteGame}>
+          delete game
+        </Button>
+      </Box>
+      <Box display="flex" flexDir="row" padding="5px">
+        <Box padding="5px">{showHero()}</Box>
+        <Box onKeyDown={handleKeyPress} tabIndex={0}>
+          {renderLevel()}
+        </Box>
+        <Box padding="5px">{showAction()}</Box>
+      </Box>
+    </Box>
+  )
 }
+
+export default LevelUi
